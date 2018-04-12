ffc4957a9858814baf0fc5a1166abf3e9b976cde
Ron Sprenkels <ron.sprenkels@gmail.com>
2018-04-12 23:22:26 +0200

    commint from liclipse gui

---
+++ ronLearnsPython/ronLearnsPython/helloWorld/helloWorld.py
@@ -0,0 +1,26 @@
+'''
+Created on Apr 12, 2018
+
+@author: rsprenkels
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
