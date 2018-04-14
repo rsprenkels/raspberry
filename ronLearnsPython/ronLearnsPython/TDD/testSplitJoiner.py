e271f2bcfd829bfb4026f769fdc7b62ab152c194
Ron Sprenkels <ron.sprenkels@gmail.com>
2018-04-14 12:47:51 +0200

    none

--- ronLearnsPython/ronLearnsPython/TDD/testSplitJoiner.py
+++ ronLearnsPython/ronLearnsPython/TDD/testSplitJoiner.py
@@ -7,19 +7,17 @@ import unittest
 
 
 class Test(unittest.TestCase):
-
+    
 
     def setUp(self):
-        pass
-
+        self.sj = SplitJoiner()
 
     def tearDown(self):
         pass
 
-
     def testName(self):
-        pass
-
+        self.assertEqual(
+            self.sj.work('work on this'), 'work-on-this', 'wrong') 
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
---
+++ ronLearnsPython/ronLearnsPython/splitJoiner.py
@@ -0,0 +1,17 @@
+'''
+Created on Apr 12, 2018
+
+@author: rsprenkels
+'''
+
+class SplitJoiner(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
