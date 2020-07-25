2430973bdf8daaa6ae6152b6b3a14dc8fc2d9bce
Ron Sprenkels <ron.sprenkels@gmail.com>
2020-07-25 16:15:36 +0200

    next

--- fresh-install/fresh-install.sh
+++ fresh-install/fresh-install.sh
@@ -1,2 +1,16 @@
 # run from user homedir (usually user is pi)
+
+# add hostame IP to /etc/hosts
+
+# copy ssh keys
+ssh-copy-id pi@pi3w
+
+# also put key_ron and key_ron.pub as id_rsa and id_rsa.pub into .ssh
+
+# on target machine, homedir
+sudo apt-get -y install git
+
+mkdir git; cd git
+git clone git@github.com:rsprenkels/raspberry.git
+
 mkdir ent
